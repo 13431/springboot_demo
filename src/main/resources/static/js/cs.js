872668ebfd1d84d61300d3a394200563f91651d1
U-RHOST\lol <monster>
2018-01-23 16:07:43 +0800

    ppp

--- src/main/resources/static/js/cs.js
+++ src/main/resources/static/js/cs.js
@@ -1,9 +1,3 @@
-
-function confirmAction(target) {
-    if (!window.confirm('您是否要删除？'))
-        event.preventDefault();
-}
-
 $(function () {
     $("[data-action]").each(function () {
         $(this).on('click', function () {
