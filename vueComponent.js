4022b6b39af1de73537330f72c25928ede892112
U-RHOST\lol <monster>
2018-03-12 13:48:12 +0800

    shiro

---
+++ vueComponent.js
@@ -0,0 +1,13 @@
+Vue.component('todo-item', {
+    template: '<div><h1>欢迎</h1><span>hello,你们好啊</span></div>'
+})
+
+
+Vue.component('my-form', {
+    template: '<div><h1>欢迎</h1><span>hello,你们好啊</span></div>'
+})
+
+
+Vue.component('my-table', {
+    template: '<div><h1>欢迎</h1><span>hello,你们好啊</span></div>'
+})
